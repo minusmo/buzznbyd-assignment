010f2e8a083e2216418e8f43b101493d25cca766
hojooneum <bldolphin96@gmail.com>
2022-12-12 22:59:19 +0900

    refactor: move IResolver to schemas

---
+++ schemas/IResolver.js
@@ -0,0 +1,32 @@
+import { buildSchema } from "graphql";
+
+export const IResolver = buildSchema(`
+    type ExchangeInfo @key(fields: ["src", "tgt"]) {
+        src: String!
+        tgt: String!
+        rate: Float!
+        date: String!
+    }
+
+    input InputDeleteExchangeInfo {
+        src: String!
+        tgt: String!
+        date: String!
+    }
+
+    input InputUpdateExchangeInfo {
+        src: String!
+        tgt: String!
+        rate: Float!
+        date: String
+    }
+
+    type Query {
+        getExchangeRate(src: String!, tgt: String!): ExchangeInfo
+    }
+
+    type Mutation {
+        postExchangeRate(info: InputUpdateExchangeInfo): ExchangeInfo
+        deleteExchangeRate(info: InputDeleteExchangeInfo): ExchangeInfo
+    }
+`);
