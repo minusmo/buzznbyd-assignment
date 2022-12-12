b98a392a8fed6f91a858a7cb468b07382bcfa2c6
hojooneum <bldolphin96@gmail.com>
2022-12-12 20:36:44 +0900

    feat: add query and mutation schema

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 import { GraphQLObjectType, GraphQLSchema, buildSchema } from "graphql";
+
+const QUERY = buildSchema(`
+    type Query {
+        "query": getExchangeRate(src: String!, tgt: String!): ExchangeInfo
+    }
+`);
+
+const MUTATION = buildSchema(`
+    type Mutation {
+        "mutation": {
+            postExchangeRate(info: InputUpdateExchangeInfo): ExchangeInfo,
+            deleteExchangeRate(info: InputDeleteExchangeInfo): ExchangeInfo
+        },
+    }
+`);
+
