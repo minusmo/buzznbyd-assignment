59133324499cbe28b10b446c90a2fbd114b8f88f
hojooneum <bldolphin96@gmail.com>
2022-12-16 23:11:40 +0900

    feat: add mongoDB connection

--- db/mongo.mjs
+++ db/mongo.mjs
@@ -1,33 +1,51 @@
+/* eslint-disable max-len */
 /* eslint-disable require-jsdoc */
-const mongo = {
-  'krw,usd': {
-    'rate': 0.0007450954094671824,
-    'date': '2022-11-28',
-  },
-  'usd,krw': {
-    'rate': 1342.11,
-    'date': '2022-11-28',
-  },
-};
+import {MongoClient, ServerApiVersion} from 'mongodb';
+
+const uri = 'mongodb+srv://superuser:gl5jXokZn5SaIDUO@buzznbyd.7qkfpyf.mongodb.net/?retryWrites=true&w=majority';
+const client = new MongoClient(
+    uri,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverApi: ServerApiVersion.v1,
+    });
 
 class MongoDB {
-  constructor() {
-    this.mongo = mongo;
+  static mongo = client
+      .db('buzznbyd')
+      .collection('ExchangeRates');
+
+  static async get(query) {
+    const result = await MongoDB.mongo.find(query).sort({'date': -1}).limit(1).toArray()[0];
+    const exchangeRate = {...result};
+    delete exchangeRate['_id'];
+    return exchangeRate;
   }
 
-  get(query) {
-    return this.mongo[query];
+  static async upsert(upsertInfo) {
+    const {src, tgt} = upsertInfo;
+    const upsertDoc = {
+      src: upsertInfo.src,
+      tgt: upsertInfo.tgt,
+      rate: upsertInfo.rate,
+      date: upsertInfo.date ? upsertInfo.date : new Date().toUTCString(),
+    };
+    const options = {upsert: true};
+    const result = await MongoDB.mongo.updateOne({src, tgt}, upsertDoc, options);
+    return result.upsertedCount > 0 ? Object.assign({}, upsertDoc) : {};
   }
 
-  upsert(query, upsertInfo) {
-    // const {rate, date} = upsertInfo;
-    return Object.assign(this.mongo[query], upsertInfo);
-  }
-
-  delete(query, deleteInfo) {
-    const result = this.mongo[query];
-    delete this.mongo[query];
-    return result;
+  static async delete(deleteInfo) {
+    const {src, tgt} = deleteInfo;
+    const findResult = await MongoDB.mongo.findOne({src, tgt});
+    let deleteResult = 0;
+    if (findResult) {
+      deleteResult = await MongoDB.mongo.deleteOne(deleteInfo);
+    }
+    const exchangeRate = {...findResult};
+    delete exchangeRate['_id'];
+    return deleteResult.deletedCount > 0 ? exchangeRate : {};
   }
 }
 
