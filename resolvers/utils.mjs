47ad620155b182bc2d87740661cf553dacb3ba2d
hojooneum <bldolphin96@gmail.com>
2022-12-16 23:09:58 +0900

    refactor: update to async function at utils

--- resolvers/utils.mjs
+++ resolvers/utils.mjs
@@ -1,19 +1,19 @@
 /* eslint-disable require-jsdoc */
 import * as ExchangeInfo from '../models/ExchangeInfo.mjs';
+import MongoDB from '../db/mongo.mjs';
 
-export function getExchangeRateFromDB(src, tgt, db) {
-  const exchangeInfo = db.get(`${src},${tgt}`); // {rate, date}
+export async function getExchangeRateFromDB({src, tgt}) {
+  const exchangeInfo = await MongoDB.get({src, tgt}); // {rate, date}
   return new ExchangeInfo.ExchangeInfo(src, tgt, exchangeInfo);
 }
 
-export function upsertExchangeRateFromDB(info, db) {
-  const {src, tgt, rate, date} = info;
-  const exchangeInfo = db.upsert(`${src},${tgt}`, {rate, date});
+export async function upsertExchangeRateFromDB(info) {
+  const exchangeInfo = await MongoDB.upsert(info);
   return new ExchangeInfo.ExchangeInfo(src, tgt, exchangeInfo);
 }
 
-export function deleteExchangeRateFromDB(info, db) {
+export async function deleteExchangeRateFromDB(info) {
   const {src, tgt, rate, date} = info;
-  const exchangeInfo = db.delete(`${src},${tgt}`, {rate, date});
+  const exchangeInfo = await MongoDB.delete(`${src},${tgt}`, {rate, date});
   return new ExchangeInfo.ExchangeInfo(src, tgt, exchangeInfo);
 }
